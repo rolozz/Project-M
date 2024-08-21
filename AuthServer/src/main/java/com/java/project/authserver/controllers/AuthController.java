207c60fed08254e3d0c58042f6a14c14a5a4f48a
sRs <rolerlozz@gmail.com>
2024-08-21 15:01:55 +0300

    commit

--- AuthServer/src/main/java/com/java/project/authserver/controllers/AuthController.java
+++ AuthServer/src/main/java/com/java/project/authserver/controllers/AuthController.java
@@ -2,45 +2,24 @@ package com.java.project.authserver.controllers;
 
 import com.java.project.authserver.dto.RequestDto;
 import com.java.project.authserver.dto.UpdateDto;
-import com.java.project.authserver.jwt.JwtUtil;
 import com.java.project.authserver.services.AuthService;
-import com.java.project.authserver.services.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class AuthController {
 
-    private final JwtUtil jwtUtil;
     private final AuthService authService;
-    private final AuthenticationManager authenticationManager;
-    private final RedisService redisService;
 
     @Autowired
-    public AuthController(JwtUtil jwtUtil, AuthService authService, AuthenticationManager authenticationManager, RedisService redisService) {
-        this.jwtUtil = jwtUtil;
+    public AuthController(AuthService authService) {
         this.authService = authService;
-        this.authenticationManager = authenticationManager;
-        this.redisService = redisService;
-    }
-
-    @PreAuthorize("hasRole('USER')")
-    @GetMapping("/redis-test")
-    public String testRedis() {
-        redisService.saveValue("testKey", "пусть это будет тут");
-        String string = redisService.getValue("testKey");
-        return "рэдис: " + string;
     }
 
     @PreAuthorize("hasRole('USER')")
@@ -57,22 +36,11 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody RequestDto requestDto) {
-        try {
-            authenticationManager.authenticate
-                    (new UsernamePasswordAuthenticationToken(
-                            requestDto.getUsername(),
-                            requestDto.getPassword())
-                    );
-        } catch (BadCredentialsException e) {
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        }
-        String token = jwtUtil.generateToken(authService.authenticateUser(requestDto));
-        redisService.saveValue(requestDto.getUsername(), token);
-        return ResponseEntity.ok(token);
+        return authService.authenticateUser(requestDto);
     }
 
     @PostMapping("/update")
-    public ResponseEntity<String> update(@RequestBody UpdateDto updateDto){
+    public ResponseEntity<String> update(@RequestBody UpdateDto updateDto) {
         authService.update(updateDto);
         return ResponseEntity.ok("Updated");
     }
