4f0a3f4ca80929abf84d4c06e7ad840955f3c74b
sRs <rolerlozz@gmail.com>
2024-08-27 14:02:28 +0300

    commit

--- ChatRoomService/src/main/java/com/java/project/chatroomservice/ChatRoomServiceApplication.java
+++ ChatRoomService/src/main/java/com/java/project/chatroomservice/ChatRoomServiceApplication.java
@@ -2,8 +2,10 @@ package com.java.project.chatroomservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ChatRoomServiceApplication {
 
     public static void main(String[] args) {
